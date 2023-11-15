7bba9d88c76fbbac3256beb90f0e46f8a3a915e5
jaredrezabala <rezabalajared@gmail.com>
2023-11-14 21:43:55 -0500

    catching up 1.2

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,32 +1,54 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+// Declare a const variable named "url" that contains the URL string of the JSON resource provided.
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
-fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        console.table(jsonObject); // temporary checking for valid response and data parsing
+// Declare a const variable named "cards" that selects the HTML div element with an id value of "cards".
+const cards = document.querySelector('#cards');
 
+// Create an async defined function named "getProphetData" to fetch data from the JSON source url using the await fetch() method.
+async function getProphetData() {
+    // Store the response from the fetch() method in a const variable named "response".
+    const response = await fetch(url);
 
-        const prophets = jsonObject['prophets'];
-        for (let i = 0; i < prophets.length; i++) {
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let bDay = document.createElement('p');
-            let bPlace = document.createElement('p');
-            let image = document.createElement('img');
+    // Convert the response to a JSON object using the .json method and store the results in a const variable named "data".
+    const data = await response.json();
 
-            image.setAttribute('src', prophets[i].imageurl);
-            image.setAttribute('alt', `${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}`);
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            bDay.textContent = 'Birthdate: ' + prophets[i].birthdate;
-            bPlace.textContent = 'Birthplace: ' + prophets[i].birthplace;
+    // Add a console.table() expression method to check the data response at this point in the console window.
+    console.table(data.prophets); // temporary testing of data retrieval
 
-            card.appendChild(h2);
-            card.appendChild(bDay);
-            card.appendChild(bPlace);
-            card.appendChild(image);
-            document.querySelector('div.cards').appendChild(card);
-        }
+    // Call the function displayProphets() with the data.prophets argument.
+    displayProphets(data.prophets);
+}
 
+// Define a function expression named "displayProphets" that handles a single parameter named "prophets" somewhere in your js file.
+// Use an arrow expression to contain statements that will process the parameter value and build a card for each prophet.
+const displayProphets = (prophets) => {
+    console.log('Displaying prophets:', prophets);
+
+    // Inside the function, use a forEach loop with the array parameter to process each "prophet" record one at a time, creating a new card each time.
+    prophets.forEach((prophet) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        // Populate the heading element with the prophet's full name using a template string to build the full name.
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        // Build the image element by setting the src, alt, loading, width, and height attributes using setAttribute().
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        // Using appendChild() on the section element named "card", add the heading and image elements one at a time.
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        // Finally, add the section card to the "cards" div that was selected at the beginning of the script file.
+        cards.appendChild(card);
     });
+};
+
+// Call the getProphetData() function to start the process.
+getProphetData();
